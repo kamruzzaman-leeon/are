4c741755b80160f959c10991f065d15715128172
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2024-07-14 13:20:09 +0600

    first commit

---
+++ src/Routers/Routers.jsx
@@ -0,0 +1,19 @@
+
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
+import Home from '../Pages/Home/Home/Home';
+
+const Routers = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+            path:"/",
+            element:<Home></Home>,
+        }
+      ]
+    },
+  ]);
+
+export default Routers;
